99ae5e861f4eaa6071230d4bccaf6de3b6412dcb
zakno52 <101570337+zakno52@users.noreply.github.com>
2025-02-06 01:40:30 +0100

    Implement JavaScript

--- src/tasksHandler/addTask.js
+++ src/tasksHandler/addTask.js
@@ -1,5 +1,13 @@
+import { handleAddTask } from "./handleAddTask";
+
 export function addTask() {
   const modalWidnow = document.getElementById("todo-modal");
+  const addbutton = document.getElementById("add-task-confirm");
+  const closebutton = document.getElementById("close-modal");
 
   modalWidnow.showModal();
+
+  addbutton.addEventListener("click", () => handleAddTask());
+
+  closebutton.addEventListener("click", () => modalWidnow.close());
 }
